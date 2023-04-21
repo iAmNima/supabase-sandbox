806ffe05afbc2431b3bf6307870dde509790b85c
Nima Bayatmoghadam <Nima.Bayatmoghadam@boc-group.com>
2023-04-21 20:51:45 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient("https://yypxuznvsmeycrfsmxsx.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl5cHh1em52c21leWNyZnNteHN4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODIwODQ1NzQsImV4cCI6MTk5NzY2MDU3NH0.0hYapMSbIEJPOBD1BSHs--QIrgPfeol5-HhPbbwv_OQ")
+
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    getCountries();
+  }, []);
+
+  async function getCountries() {
+    const { data } = await supabase.from("countries").select();
+    setCountries(data);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {countries.map((country) => (
+        <li key={country.name}>{country.name}</li>
+      ))}
+    </ul>
   );
 }
 
